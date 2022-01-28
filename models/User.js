eced8b8bcc2e0fb0c9ca6bb68500c1f9fabbfffd
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-01-27 16:37:38 -0800

    Created the models and db connection

---
+++ models/User.js
@@ -0,0 +1,55 @@
+const { Schema, model} = require('mongoose')
+
+
+const userSchema = new Schema(
+    {
+        userame: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+             unique: true,
+            trim: true,
+            lowercase: true,
+            validate: {
+                validator: function(v) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+                },
+                message: "Please enter a valid email"
+            },
+            required: [true, "Email required"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    })
+
+const User = model('user', userSchema);
+
+module.exports = User;
